1fac83dc8ac6330d891f735a482178712e796a56
danielgreen229 <69796780+danielgreen229@users.noreply.github.com>
2022-01-22 21:19:19 +0400

    master

---
+++ src/components/option.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="option__container" >
+    <div class="option__image" 
+      :style="{ 'background-image': `url(${option.avatar ? option.avatar : null})` , 
+                'background-color': `${option.avatar ? 'transparent' : 'grey' }`}">
+    </div>
+      <span class="option__desc">
+        <span class="option__title">{{ option.name ? option.name : option.alias }}</span>
+        <span class="option__span">{{ option.type == "user" ? '@' + option.alias : 'Компания' }}</span>
+      </span>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  props: {
+    option: Object
+  },
+  methods: {
+    
+  }
+};
+</script>
+<style scoped> 
+.option__container {
+  display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-start;
+}
+.option__image {
+  height: 2em;
+  width: 2em;
+  border-radius: 0.3em;
+  background-repeat: no-repeat;
+    background-size: contain;
+}
+.option__desc {
+  display: flex;
+  flex-direction: column;
+  margin-left: 0.5em;
+}
+.option__title {
+  color: black;
+}
+.option__span {
+  color: #8e8e8e;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,111 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home">   
+   	<div>
+		<multiselect 
+		 	v-model="value" 
+		 	:options="countries" 
+		 	placeholder="Select one" 
+		 	:custom-label="nameWithLang"
+		 	:searchable="true" 
+		  	:loading="isLoading" 
+		  	:internal-search="false" 
+		  	:clear-on-search="true"
+		  	:clear-on-select="true" 
+		  	:close-on-select="true" 
+		  	:options-limit="300" 
+		  	:limit="3" 
+		 	track-by="alias"
+		 	:max-height="220" 
+		  	:show-no-results="true" 
+		  	:hide-selected="true" 
+		  	@search-change="asyncFind"
+		  	selectLabel=""
+		  	:multiple="true"
+		  	@select="selectOption(value)"
+		>
+
+	  		<template slot="option" slot-scope="props">
+	  			<searchOption :option="props.option"/>
+	  		</template>
+
+	  		<span slot="noResult">Oops! No elements found. Consider changing the search query.</span>
+		</multiselect>
+
+
+	
+	</div>
+
+
   </div>
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
+import Multiselect from 'vue-multiselect'
+import 'vue-multiselect/dist/vue-multiselect.min.css'
+import searchOption from '../components/option.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  	name: 'Home',
+  	components: { Multiselect, searchOption },
+   	data() {
+      return {
+       	value: null,
+       	selectedCountries: [],
+        countries: [],
+        isLoading: false
+      }
+  	},
+  	methods: {
+  		nameWithLang ({ name, alias }) {
+	      	return  alias ? '@' + alias : name
+	    },
+   		clearSelected () {
+   			this.selectedCountries = []
+   		},
+	    asyncFind (query) {
+	      if(query.length > 2) {
+	      	this.isLoading = true
+	      	axios.get("https://habr.com/kek/v2/publication/suggest-mention?q=" + query)
+          	.then(res => {
+          		this.countries = res.data.data
+          		this.isLoading = false
+          	})
+          	.catch(err => console.log(err));
+	      }	
+	    },
+	    selectOption (value) {
+	    	if(this.value != null && this.value.length > 0) {
+	    		this.value[0] = this.value[1]
+	    		this.value.pop()
+	    	}
+	    },
+	    clearAll () {
+	      this.selectedCountries = []
+	    }
+   	},
+   	created() {
+   		this.asyncFind('aaa')
+   	}
+ 
 }
 </script>
+<style scoped>
+>>>.multiselect__tag {
+	background: #91a8ba;
+	margin-bottom: 0px;
+}
+>>>.multiselect__tag-icon:after {
+	color: white;
+}
+>>>.multiselect__tag-icon {
+	background-color: transparent;
+}
+>>>.option__single-label {
+	color: white;
+}
+>>>.multiselect__option--highlight {
+	background-color: #eeeeee;
+}
+</style>
